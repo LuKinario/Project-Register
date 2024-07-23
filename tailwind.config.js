915ea925b3c94bf3cc1ede805a9474c2517456cd
LuKinario <163080941+LuKinario@users.noreply.github.com>
2024-07-23 13:24:23 -0300

    Subindo Ambiente

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
